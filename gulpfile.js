4da23d8188e795116e8175254c7674dad2137386
”Serj” <serjvertelko@gmail.com>
2022-03-23 01:26:15 +0300

    my gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+
+//импорт общих плагинов
+import {plugins} from "./gulp/config/plugins.js";
+
+// Передаём значение в глобальную переменную
+global.app = {
+ path: path,
+ gulp: gulp,
+ plugins:plugins
+}
+//импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import {reset} from "./gulp/tasks/reset.js";
+import {html} from "./gulp/tasks/html.js";
+import {server} from"./gulp/tasks/server.js";
+
+// наблюдатель за изменениями в файлах
+function watcher(){
+ gulp.watch(path.watch.files,copy);
+ gulp.watch(path.watch.html,html);
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+//построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+
+//выполнение сценария по умолчанию
+gulp.task('default', dev);
